4f45a0284f091df367cdd560df6bd2d1c77830f7
ktragng <126523634+ktragng@users.noreply.github.com>
2026-08-28 10:56:15 +0930

    Update streamlit_app.py
    connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,9 @@ st.write("The name on your smoothie will be: ", name_on_order)
 
 
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect(
